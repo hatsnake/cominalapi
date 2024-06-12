8acf7a4d5d7dd22d1de1cdd7b8ff8031346b657a
hatsnake <60315837+hatsnake@users.noreply.github.com>
2024-06-12 23:43:19 +0900

    MySQL 연결 및 메인 페이지 테스트
    MySQL 연결 및 메인 페이지 테스트

---
+++ src/main/java/com/hatsnake/cominalapi/test/TestController.java
@@ -0,0 +1,12 @@
+package com.hatsnake.cominalapi.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String main() {
+        return "index.html";
+    }
+}
